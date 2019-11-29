0b8e7524c462a8427b465037e652495b7fbe23a6
chayou20 <chayou20@naver.com>
2019-11-29 14:01:50 +0900

    SetAdapterNumber(int iNum)으로 수정, GUI에서 index에 따른 Adapter 설정을 할 수 있게함. Adapter에 따른 ip 주소와 mac주소 저장. 다른 계층에서 Get함수로 접근이 가능함. device에 따른 ip 반환 함수 구현.

--- src/ARP/NILayer.java
+++ src/ARP/NILayer.java
@@ -1,21 +1,22 @@
 package ARP;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.nio.ByteBuffer;
-import java.text.DateFormat.Field;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapAddr;
 import org.jnetpcap.PcapIf;
+import org.jnetpcap.PcapSockAddr;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
 
 
 public class NILayer implements BaseLayer {
+	
+
 	public int nUpperLayerCount = 0;
 	public String pLayerName = null;
 	public BaseLayer p_UnderLayer = null;
@@ -26,16 +27,22 @@ public class NILayer implements BaseLayer {
 	public PcapIf device;
 	public List<PcapIf> m_pAdapterList;
 	StringBuilder errbuf = new StringBuilder();
+	
+	private int ipAddr;
+	private byte[] macAddr;
 
 	public NILayer(String pName) {
+		
 		pLayerName = pName;
+		
 		m_pAdapterList = new ArrayList<PcapIf>();
 		m_iNumAdapter = 0;
+		
 		SetAdapterList();
-
 	}
 
 	public void SetAdapterList() {
+		
 		int r = Pcap.findAllDevs(m_pAdapterList, errbuf);
 		if (r == Pcap.NOT_OK || m_pAdapterList.isEmpty()) {
 			System.err.printf("Can't read list of devices, error is %s", errbuf.toString());
@@ -64,8 +71,19 @@ public class NILayer implements BaseLayer {
 ////		PacketStarDriver();
 ////		Receive();
 //	}
-	
-	public void SetAdapterNumber() throws IOException {
+
+	// GUI에서 호출, AdapterIndex를 넘겨받는다.
+	public void SetAdapterNumber(int iNum) throws IOException {
+		
+		m_iNumAdapter = iNum;
+		
+		device = this.m_pAdapterList.get(m_iNumAdapter);
+		
+		ipAddr = GetDeviceIpAddress(device);
+		macAddr = device.getHardwareAddress();
+		
+		
+		/*
 		// 로컬 IP취득
 		InetAddress ip = InetAddress.getLocalHost();
 		byte[] mac = null;
@@ -76,6 +94,7 @@ public class NILayer implements BaseLayer {
 
 		// 네트워크 인터페이스가 NULL이 아니면
 		if (netif != null) {
+			
 			// 네트워크 인터페이스 표시명 출력
 			System.out.print(netif.getDisplayName() + " : ");
 					
@@ -94,10 +113,37 @@ public class NILayer implements BaseLayer {
 					}
 				}
 			}
-		}
+		}*/
+		
 		PacketStarDriver();
 	    Receive();
 	   }
+	
+	
+	//IP Address를 반환한다.
+	private int GetDeviceIpAddress(PcapIf dev) {
+		List<PcapAddr> addrs = dev.getAddresses();
+	    ListIterator<PcapAddr> it = addrs.listIterator();
+	    
+	    PcapAddr ipv4 = null;
+	    
+	    while (it.hasNext()) {
+	        PcapAddr temp = it.next();
+	        if (temp.getAddr().getFamily() == PcapSockAddr.AF_INET) {
+	            ipv4 = temp;
+	        }
+	    }
+	    
+	    if (ipv4 == null) {
+	        return -1;
+	    } else {
+	        byte[] ipv4byte = ipv4.getAddr().getData();
+	        assert (ipv4byte.length == 4);
+	        int retv = 0;
+	        retv = ((ipv4byte[0] << 24) & 0xff000000) | ((ipv4byte[1] << 16) & 0x00ff0000) | ((ipv4byte[2] << 8) & 0x0000ff00) | (ipv4byte[3] & 0x000000ff);
+	        return retv;
+	    }
+	}
 
 	public void PacketStarDriver() {
 		// TODO Auto-generated method stub
@@ -105,6 +151,16 @@ public class NILayer implements BaseLayer {
 		int flags = Pcap.MODE_PROMISCUOUS;
 		int timeout = 10 * 1000;
 		m_AdapterObject = Pcap.openLive(m_pAdapterList.get(m_iNumAdapter).getName(), snaplen, flags, timeout, errbuf);
+	}
+	
+	public boolean Receive() {
+
+		// TODO Auto-generated method stub
+		Receive_Thread thread = new Receive_Thread(m_AdapterObject, this.GetUpperLayer(0));
+		Thread obj = new Thread(thread);
+		obj.start();
+		System.out.println("NI");
+		return false;
 	}
 
 	public boolean Send(byte[] input, int length) {
@@ -115,15 +171,15 @@ public class NILayer implements BaseLayer {
 		}
 		return true;
 	}
-
-	public boolean Receive() {
-		// TODO Auto-generated method stub
-		Receive_Thread thread = new Receive_Thread(m_AdapterObject, this.GetUpperLayer(0));
-		Thread obj = new Thread(thread);
-		obj.start();
-		System.out.println("NI");
-		return false;
+	
+	public int GetIpAddress(){
+		return this.ipAddr;
 	}
+	
+	public byte GetMacAddress(){
+		return this.macAddr;
+	}
+	
 
 	@Override
 	public String GetLayerName() {
@@ -177,6 +233,7 @@ public class NILayer implements BaseLayer {
 }
 
 class Receive_Thread implements Runnable {
+	
 	byte[] data;
 	Pcap AdapterObject;
 	BaseLayer UpperLayer;
@@ -194,7 +251,9 @@ class Receive_Thread implements Runnable {
 					UpperLayer.Receive(data);
 				}
 			};
+			
 			AdapterObject.loop(100000, jpacketHandler, "");
+		
 		}
 	}
 }
